fec6b373149124ce6b046480642cfbc9b29f07c8
Eamon Boyle <blaowskate@hotmail.com>
2023-11-05 13:05:34 +0000

    End of Section 4

---
+++ src/AuctionService/Consumers/AuctionCreatedFaultConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using Contracts;
+
+namespace AuctionService.Consumers;
+
+public class AuctionCreatedFaultConsumer : IConsumer<Fault<AuctionCreated>>
+{
+    public async Task Consume(ConsumeContext<Fault<AuctionCreated>> context)
+    {
+        Console.WriteLine("--> Conmsuming fault: " + context.Message.Message.Id);
+
+        var exception = context.Message.Exceptions.First();
+
+        if (exception.ExceptionType == "System.ArgumentException")
+        {
+            context.Message.Message.Model = "FooBar";
+            await context.Publish(context.Message.Message);
+        }
+        else
+        {
+            Console.WriteLine("Not an argument exception - update error dashboard somewhere");
+        }
+    }
+}
---
+++ src/SearchService/Consumers/AuctionDeletedConsumer.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class AuctionDeletedConsumer : IConsumer<AuctionDeleted>
+{
+    public async Task Consume(ConsumeContext<AuctionDeleted> context)
+    {
+        Console.WriteLine("--> Consuming AuctionDeleted: {0}", context.Message.Id);
+
+        var result = await DB.DeleteAsync<Item>(context.Message.Id);
+
+        if (!result.IsAcknowledged)
+        {
+            throw new MessageException(typeof(AuctionDeleted), $"Could not delete item with id: {context.Message.Id}");
+        }
+    }
+}
